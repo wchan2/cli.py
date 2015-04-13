d4ac26b199434f772668a601ed2bde2991037e02
Will Chan <willchan2@gmail.com>
2015-04-13 18:37:06 -0400

    refactor to have the command to parse the cli args into flags

--- cli/app.py
+++ cli/app.py
@@ -1,6 +1,5 @@
 import sys
 from collections import defaultdict
-from cli.context import Context
 
 class App:
     def __init__(self, **kwargs):
@@ -17,39 +16,17 @@ class App:
 
     def run(self, cli_args):
         if len(cli_args) < 2:
+            print('command name required')
             self.print_help()
             sys.exit(1)
         command = self._find_command(cli_args[1])
         if not command:
             self.print_help()
-            sys.exit(1)    
-        arguments = self._build_arguments(cli_args)
-        context = self._build_context(command.flags, arguments)
-        command.execute(context)
+            sys.exit(1)
+        command.execute(cli_args)
 
     def _find_command(self, cmd_name):
         found_commands = list(filter(lambda cmd: cmd.name == cmd_name, self.commands))
         if len(found_commands) == 1:
             return found_commands[0]
         return None
-
-    def _build_arguments(self, cli_args):
-        arguments = defaultdict(lambda: None)
-        for i, arg in enumerate(cli_args):
-            if arg.startswith('-'):
-                try:
-                    arguments[arg.replace('-', '')] = cli_args[i + 1]
-                except:
-                    print('%s flag specified without a value\n--' % arg.replace('-', ''))
-                    self.print_help()
-                    sys.exit(1)
-        return arguments
-
-    def _build_context(self, flags, arguments):
-        context = Context()
-        for flag in flags:
-            if arguments[flag.name]:
-                context.set(flag.name, arguments[flag.name])
-            else:
-                context.set(flag.name, flag.value)
-        return context
--- cli/command.py
+++ cli/command.py
@@ -1,11 +1,25 @@
+from collections import defaultdict
+from cli.context import Context
+
 class Command:
     def __init__(self, name, description, flags=[]):
         self.name = name
         self.flags = flags
         self.description = description
         
-    def execute(self, context):
+    def execute(self, cli_arguments):
+        arguments = self._parse_arguments(cli_arguments)
+        context = self.parse_context(arguments)
         self.action(context)
+
+    def parse_context(self, cli_arguments):
+        context = Context()
+        for flag in self.flags:
+            if cli_arguments[flag.name]:
+                context.set(flag.name, cli_arguments[flag.name])
+            else:
+                context.set(flag.name, flag.value)
+        return context
 
     def __call__(self, fn):
         self.action = fn
@@ -18,3 +32,15 @@ class Command:
         help_text.append('flags:')
         help_text.append('\n'.join(flag_strings))
         return '\n'.join(help_text)
+
+    def _parse_arguments(self, cli_arguments):
+        arguments = defaultdict(lambda: None)
+        for i, arg in enumerate(cli_arguments):
+            if arg.startswith('-'):
+                try:
+                    arguments[arg.replace('-', '')] = cli_arguments[i + 1]
+                except:
+                    print('%s flag specified without a value\n--' % arg.replace('-', ''))
+                    self.print_help()
+                    sys.exit(1)
+        return arguments
