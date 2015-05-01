316eafce8bcdbb81f3dcf59f0be0413dc2fbe703
Will Chan <willchan2@gmail.com>
2015-05-01 04:42:44 -0400

    add tests for #create_parser

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,29 +8,42 @@ class TestAppCommandWithNoFlags(unittest.TestCase):
     def setUp(self):
         self.app = App(name='app name', description='app description')
 
+        self.command_mock_without_flags = Mock()
+        self.command_mock_without_flags.execute = Mock(return_value=None)
+        self.command_mock_without_flags.configure_mock(name='test_command', description='test description', flags=[])
+
+        flag_mock = Mock()
+        flag_mock.configure_mock(name='test_flag', description='flag_value')
+        self.command_mock_with_flag = Mock()
+        self.command_mock_with_flag.execute = Mock(return_value=None)
+        self.command_mock_with_flag.configure_mock(name='test_command', description='test description', flags=[flag_mock])
+
+        self.app.command(self.command_mock_without_flags)
+
         # mock the print statements so it doesn't actually output to stdout
         self.app.print_help = Mock()
 
-        self.command = Command('test_command', 'test description')
-        self.command.execute = Mock(return_value=None)
-        self.app.command(self.command)
-
     def tearDown(self):
         self.app = None
-        self.command = None
+        self.command_mock_without_flags = None
+        self.command_mock_with_flag = None
 
     def test_app_name(self):
-        self.assertEquals(self.app.name, 'app name', 'app name is the proper value')
+        self.assertEqual(self.app.name, 'app name', 'app name is the proper value')
 
     def test_app_description(self):
-        self.assertEquals(self.app.description, 'app description', 'app description is the proper value')
+        self.assertEqual(self.app.description, 'app description', 'app description is the proper value')
 
     def test_app_commands(self):
-        self.assertEquals(self.app.commands[0], self.command, 'the command is saved in the app\'s command list')
+        self.assertEqual(self.app.commands[0], self.command_mock_without_flags, 'the command is saved in the app\'s command list')
+
+    def test_app_create_parser(self):
+        parsed_args = self.app.create_parser(self.command_mock_with_flag).parse_args(['test_command', '-test_flag', 'test flag value'])
+        self.assertEqual(parsed_args.test_flag, 'test flag value', 'creates a parser that parses the correct value for a given flag')
         
     def test_app_run_with_found_command(self):
         self.app.run(['command.py', 'test_command'])
-        self.assertTrue(self.command.execute.called, 'command is found and executed')
+        self.assertTrue(self.command_mock_without_flags.execute.called, 'command is found and executed')
 
     def test_app_run_with_unfound_command(self):
         with self.assertRaises(SystemExit):
@@ -42,19 +55,28 @@ class TestAppCommandWithNoFlags(unittest.TestCase):
 
     def test_app_command_executed_with_no_flags(self):
         self.app.run(['command.py', 'test_command'])
-        self.assertTrue(self.command.execute.called, 'parses arguments')
+        self.assertTrue(self.command_mock_without_flags.execute.called, 'parses arguments')
 
 class TestAppCommandWithFlags(TestAppCommandWithNoFlags):
     def setUp(self):
         super(TestAppCommandWithFlags, self).setUp()
-        self.command.flags = [Flag('flag1', 'flag 1 desc'), Flag('flag2', 'flag 2 desc')]
+
+        mock_flag_1 = Mock()
+        mock_flag_2 = Mock()
+
+        mock_flag_1.configure_mock(name='flag1', description='flag 1 desc')
+        mock_flag_2.configure_mock(name='flag2', description='flag 2 desc')
+        self.flag_mocks = [mock_flag_1, mock_flag_2]
 
     def test_cli_args_does_not_include_flags(self):
-        self.app.run(['command.py', 'test_command', '-flag1', 'flag 1 value', '--flag2', 'flag 2 value'])
-        context = self.command.execute.call_args[0][0]
+        with unittest.mock.patch.object(self.command_mock_with_flag, 'flags', self.flag_mocks):
+            self.app.commands = [self.command_mock_with_flag]
+            self.app.run(['command.py', 'test_command', '-flag1', 'flag 1 value', '--flag2', 'flag 2 value'])
 
-        self.assertEqual(context.get('flag1'), 'flag 1 value', 'the first flag is the proper value')
-        self.assertEqual(context.get('flag2'), 'flag 2 value', 'the second flag is the proper value')
+            context = self.command_mock_with_flag.execute.call_args[0][0]
+
+            self.assertEqual(context.get('flag1'), 'flag 1 value', 'the first flag is the proper value')
+            self.assertEqual(context.get('flag2'), 'flag 2 value', 'the second flag is the proper value')
 
 if __name__ == '__main__':
     unittest.main()
